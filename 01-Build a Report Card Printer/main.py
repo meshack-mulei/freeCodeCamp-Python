6333b7b44b4c34008e9f3b663202d688d0812746
meshack-mulei <meshackmulei8@gmail.com>
2026-07-10 19:57:46 +0300

    Completed Workshop 1

--- 01-Build a Report Card Printer/main.py
+++ 01-Build a Report Card Printer/main.py
@@ -10,3 +10,9 @@ print(age, type(age))
 score = 80.5
 print(isinstance(score, float))
 print(score, type(score))
+# Output:
+# Alice <class 'str'>
+# True <class 'bool'>
+# 20 <class 'int'>
+# True
+# 80.5 <class 'float'>
